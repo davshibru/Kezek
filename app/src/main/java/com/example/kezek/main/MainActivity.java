c675547c3b198d9aa28a22eba3efc2a631779396
David Shibru <davshibru@gmail.com>
2021-03-13 09:31:35 +0300

    Initial commit

---
+++ app/src/main/java/com/example/kezek/main/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.kezek.main;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.kezek.R;
+import com.example.kezek.fragments.AccountFragment;
+import com.example.kezek.fragments.CardFragment;
+import com.example.kezek.fragments.ChatFragment;
+import com.example.kezek.fragments.MapFragment;
+import com.example.kezek.fragments.TaskFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListner);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new MapFragment()).commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListner =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()){
+                        case R.id.map:
+                            selectedFragment = new MapFragment();
+                            break;
+                        case R.id.chat:
+                            selectedFragment = new ChatFragment();
+                            break;
+                        case R.id.list:
+                            selectedFragment = new TaskFragment();
+                            break;
+                        case R.id.card:
+                            selectedFragment = new CardFragment();
+                            break;
+                        case R.id.account:
+                            selectedFragment = new AccountFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+
+}
